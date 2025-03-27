da4867fd8a2551a9fecda1c917a4248b19ab344a
safir <safir.kabidi7@etu.univ-lorraine.fr>
2025-03-27 10:29:55 +0100

    Form Avis

---
+++ src/Form/AvisType.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note', IntegerType::class, [
+                'label' => 'Note (1 à 5)',
+                'attr' => ['min' => 1, 'max' => 5],
+            ])
+            ->add('commentaire', TextareaType::class, [
+                'label' => 'Votre avis',
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Envoyer']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
